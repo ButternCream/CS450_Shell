a13dae618e2fe5edb06969833de0b709aa433878
ButternCream <mitchsco@sonoma.edu>
2017-10-25 14:05:34 -0700

    Finished. Yay for copy paste

--- shell/main.c
+++ shell/main.c
@@ -2,159 +2,149 @@
 #include "constants.h"
 #include "parsetools.h"
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <stdbool.h>
 
-void create_command(char*[], char*[], int start, int end);
-void exec_output(char*[], char*);
-void exec_input(char*[], char*);
-void exec_pipe(char*[], char*[]);
-int nextDelimPos(char*[],int, int);
-bool isDelim(char*);
+#define STDINPUT 0
+#define STDOUTPUT 1
+#define NUM(a) (sizeof(a) / sizeof(a[0]))
+#define ERROR -1
 
+void print(char ** arr);
+void exec_output(char * output);
+void exec_input(char * input);
+int exec_command(char ** cmd, int num_words);
+int input, output;
+
+void syserror(const char * s)
+{
+    extern int errno;
+
+    fprintf(stderr, "%s\n", s);
+    fprintf(stderr, "(%s)\n", strerror(errno));
+    exit(1);
+}
 
 int main() {
     // Buffer for reading one line of input
     char line[MAX_LINE_CHARS];
     char* line_words[MAX_LINE_WORDS + 1];
+    int num_words = 0;
+    
     // Loop until user hits Ctrl-D (end of input)
     // or some other input error occurs
     while( fgets(line, MAX_LINE_CHARS, stdin) ) {
-        // Num words = the input
-        int num_words = split_cmd_line(line, line_words);
-        char* argv[MAX_LINE_WORDS];
-        int pos = -1;
+        num_words = split_cmd_line(line, line_words);
+        int cmdidx = 0;
+        input = 0;
+        output = 1;
+        char* command[num_words];
+        int stdincopy;
+        int stdoutcopy;
+        stdincopy = dup(STDINPUT);
+        stdoutcopy = dup(STDOUTPUT);
+        if (stdincopy == ERROR || stdoutcopy == ERROR)
+            syserror("Error copying stdin or stdout\n");
+        for (int i = 0; i < num_words; i++) {
+	        if ((strcmp(line_words[i], ">")) == 0) {
+                exec_output(line_words[i+1]);
+                i++;
+            }
+            else if ((strcmp(line_words[i], "<")) == 0) {
+                stdincopy = dup(STDINPUT);
+                if (stdincopy == ERROR)
+                    syserror("Error copying stdin\n");
+                exec_input(line_words[i+1]);
+                i++;
+            }
+            else if ((strcmp(line_words[i], "|")) == 0) {
+                stdincopy = dup(STDINPUT);
+                stdoutcopy = dup(STDOUTPUT);
+                if (stdincopy == ERROR || stdoutcopy == ERROR)
+                    syserror("Error copying stdin or stdout\n");
+                int pfd[2];
+                if (pipe(pfd) == ERROR)
+                    syserror("There was an error piping\n");
 
-        // Just for demonstration purposes
-        for (int i=0; i < num_words; i++)
-        {
-            // Testing only output redirection. Need to fix for adding pipes and nested redirection
-            if (strcmp(line_words[i], ">") == 0)
-            {
-                // Build the command before the '>'
-                char* built[MAX_LINE_WORDS+1];
-                create_command(built, line_words, 0, i);
-                // Execute output redirection
-                exec_output(built, line_words[i+1]);
-                break;
+                output = pfd[1];
+                exec_command(command, cmdidx);
+                cmdidx = 0;
+                if (close(pfd[1]) == ERROR)
+                    syserror("Error closing pfd[1]\n");
+                dup2(stdoutcopy, STDOUTPUT);
+                output = 1;
+
+                input = pfd[0];
             }
-            else if (strcmp(line_words[i], "<") == 0)
-            {
-                // Build the command before the '<'
-                char* built[MAX_LINE_WORDS+1];
-                create_command(built, line_words, 0, i);
-                // Execute the input redirection
-                exec_input(built, line_words[i+1]);
-                break;
-            }
-            else if (strcmp(line_words[i], "|") == 0)
-            {
-                // Create arrays for both pipe commands
-                char* built[MAX_LINE_WORDS+1];
-                char* built2[MAX_LINE_WORDS+1];
-                // Create command on left side of |
-                create_command(built, line_words, 0, i);
-                // Find the next delimiter (<,>,|)
-                int next = nextDelimPos(line_words, num_words, i+1);  
-                // Create the command on right side of |
-                create_command(built2, line_words, i+1, next);
-                // Execute pipes with both commands
-                exec_pipe(built, built2);
-                break;
-            }
-            else if (i == num_words - 1 && fork() == 0)
-            {
-                // Build the command
-                char* built[MAX_LINE_WORDS+1];
-                create_command(built, line_words,0,num_words);
-                // Execute and single command, i.e ls, ls -al, who, etc...
-                execvp(built[0], built);
-                break;
+            else {
+                command[cmdidx++] = line_words[i];
             }
         }
+        exec_command(command, cmdidx);
+        if (input != 0)
+            dup2(stdincopy, STDINPUT);
+        if (output != 1)
+            dup2(stdoutcopy, STDOUTPUT);
+        if (close(stdoutcopy) == ERROR)
+            syserror("Error closing stdoutcopy\n");
+        if (close(stdincopy) == ERROR)
+            syserror("Error closing stdincopy\n");
     }
-
+    wait(NULL);
+    if (close(STDINPUT) == ERROR)
+        syserror("Error closing stdinput\n");
+    if (close(STDOUTPUT) == ERROR)
+        syserror("Error closing stdoutput\n");
     return 0;
 }
 
-// Checks if a string is a delimeter
-bool isDelim(char* c)
-{
-        if (strcmp(c, ">") == 0) return true;
-        if (strcmp(c, "<") == 0) return true;
-        if (strcmp(c, "|") == 0) return true;
-        return false;
+void exec_output(char * filename) {
+    output = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    if (output == ERROR)
+        syserror("Error opening file for output\n");
 }
 
-// Find the next delimeter position given the starting index
-int nextDelimPos(char* words[], int num_words, int start)
-{
-    int temp = start;
-    while (temp < num_words && !isDelim(words[temp]))
-    {
-        temp++;
-    }  
-    return temp;
+void exec_input(char * _input) {
+    input = open(_input, O_RDONLY);
+    if (input == ERROR)
+        syserror("Error opening file for input\n");
 }
 
-// Create commands array for execvp with the first param 'built' since it complains about returning
-// pointers, example built = ["ls", "-al"]
-void create_command(char* built[], char* words[], int start, int end)
-{
-    for(int i = 0; i < end-start; i++)
-    {
-        built[i] = words[start+i];
+int exec_command(char ** cmd, int num_words) {
+  int pid = fork();
+  if (pid < 0)
+        syserror("Error creating fork\n");
+  cmd[num_words] = (char *) NULL;
+    if (pid == 0) {
+        if (input != 0) {
+            dup2(input, 0);
+            if (close(input) == ERROR)
+                syserror("Error closing input\n");
+        }
+        if (output != 1) {
+            dup2(output, 1);
+            if (close(output) == ERROR)
+                syserror("Error closing output\n");
+        }
+        if (num_words > 1) {
+	        execvp(cmd[0], cmd);
+        }
+        else {
+            execlp(cmd[0], cmd[0], (char *)NULL);
+        }
     }
+    
+    return pid;
 }
 
-// Execute single output redirection
-void exec_output(char* command[], char* filename)
-{
-    if (fork() == 0)
-    {
-        int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-        //stdout
-        dup2(fd, 1);
-        close(fd);
-        execvp(command[0], command); 
+void print(char ** arr) {
+//    printf("Printing array\n");
+    for (int i = 0; i < NUM(arr); i++) {
+        printf("%s ", arr[i]);
     }
-}
-
-// Execute single input redirection
-void exec_input(char* command[], char* input)
-{
-    if (fork() == 0)
-    {
-        int fd = open(input, O_RDONLY);
-        dup2(fd,0);
-        close(fd);
-        execvp(command[0], command);
-    }
-}
-
-// Execute single pipe
-void exec_pipe(char* writer[], char* reader[])
-{
-    int pfd[2];
-    pipe(pfd);
-    if (fork() == 0)
-    {
-        dup2(pfd[0], 0);
-        close(pfd[0]);
-        close(pfd[1]);
-        execvp(reader[0], reader);
-    }
-    if (fork() == 0)
-    {
-        dup2(pfd[1], 1);
-        close(pfd[0]);
-        close(pfd[1]);
-        execvp(writer[0], writer);
-    }
-    close(pfd[0]);
-    close(pfd[1]);
-    wait(NULL);
+    printf("\n");
 }
